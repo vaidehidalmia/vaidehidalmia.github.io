f450ee574bc23e96436308219e170c62230df539
Vaidehi Dalmia <vd2302@columbia.edu>
2018-01-30 17:33:28 -0500

    Added time

--- sketch.js
+++ sketch.js
@@ -3,12 +3,19 @@ function setup() {
 }
 
 function draw() {
-  fill(255,0,0);
-  ellipse(100, 100, 80, 80);
+	
+
   fill(255,204,0);
-  rect(250,70,80,80);
+  rect(300,70,80,map(second(), 0, 60, 0, 80));
   
   textSize(50);
   fill(0,0,255);
   text('Hi This is Vaidehi',40,280);
+
+  // First change the mode to degrees (default is radians)
+  angleMode(DEGREES);
+  // Map the function minute() to values from 0~360
+  rotate(map(second(), 60, 90, 80, 360));
+  fill(255,0,0);
+  ellipse(200, 200, 80, 80);
 }
